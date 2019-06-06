9bfae67d4ebeeabc8d4103ff9c9ed4ffba0b4c70
Rob Prouse <rob@prouse.org>
2019-06-06 11:18:23 -0400

    Initial import

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
