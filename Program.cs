a6e1f3d7e9b5457235fbb20e53c4fbae60fe4794
Rob Prouse <rob@prouse.org>
2019-06-06 11:39:38 -0400

    Generates a code word from the embedded resources

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace CodeNames
 {
     class Program
     {
+        const string NOUNS = "CodeNames.nouns.txt";
+        const string ADJECTIVES = "CodeNames.adjectives.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nouns = ReadResourceFile(NOUNS).ToArray();
+            var adjs = ReadResourceFile(ADJECTIVES).ToArray();
+            var rand = new Random();
+
+            var noun = nouns[rand.Next(0, nouns.Length)];
+            var adj = adjs[rand.Next(0, adjs.Length)];
+
+            Console.WriteLine($"{adj.ToFirstUpper()} {noun.ToFirstUpper()}");
+        }
+
+        static IEnumerable<string> ReadResourceFile(string resource)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            using (Stream stream = asm.GetManifestResourceStream(resource))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string line = reader.ReadLine();
+                while (!string.IsNullOrWhiteSpace(line))
+                {
+                    yield return line;
+                    line = reader.ReadLine();
+                }
+            }
         }
     }
 }
